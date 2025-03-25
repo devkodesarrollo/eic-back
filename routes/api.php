8118a94d5894f85b1639575b734a1a633cd67eed
omcortina <oscarcortina12@gmail.com>
2025-03-25 18:22:34 -0500

    Api para listar todos los roles del sistema

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -16,6 +17,10 @@ Route::post('login', [AuthController::class, 'login']);
         Route::put('/{id}', [UserController::class, 'update']);
         Route::patch('/{id}', [UserController::class, 'changeField']);
         Route::delete('/{id}', [UserController::class, 'delete']);
+    });
+
+    Route::group(['prefix' => 'role'], function () {
+        Route::get('', [RoleController::class, 'all']);
     });
     
 // });
